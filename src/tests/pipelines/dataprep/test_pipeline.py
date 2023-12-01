b5638b583bbf85905803477738d4150224a0c4e8
Leonardo Brettas <leonardo.brettas@cargosapiens.com.br>
2023-11-30 23:58:40 -0300

    Explanatory Commit

--- src/tests/pipelines/dataprep/test_pipeline.py
+++ src/tests/pipelines/dataprep/test_pipeline.py
@@ -1,41 +1,33 @@
-"""
-This is a boilerplate test file for pipeline 'dataprep'
-generated using Kedro 0.18.14.
-Please add your pipeline tests here.
-
-Kedro recommends using `pytest` framework, more info about it can be found
-in the official documentation:
-https://docs.pytest.org/en/latest/getting-started.html
-"""
 import pytest
-from freight_cost.pipelines.dataprep.nodes import preprocess_prices, preprocess_distances, preprocess_regression_table, feature_engineer_regression_table, split_data
-from kedro.pipeline import Pipeline
-import pandas as pd
-
+from freight_cost.pipelines.dataprep.nodes import (
+    preprocess_prices, 
+    preprocess_distances, 
+    preprocess_regression_table, 
+    feature_engineer_regression_table, 
+    split_data
+)
+from pandas.testing import assert_frame_equal
+from pandas import DataFrame
 
 def test_preprocess_prices():
-    # Create a sample DataFrame for testing
-    prices = pd.DataFrame({
+    prices = DataFrame({
         'Flow': ['Flow1', 'Flow2'],
         'Origem': ['Origem1', 'Origem2'],
         'Destino': ['Destino1', 'Destino2'],
         'Veículo': ['Veículo1', 'Veículo2'],
         'Tarifa por Embarque': [10, 20]
     })
-    # Call the function to preprocess the prices DataFrame
     processed_prices = preprocess_prices(prices)
-
-    # Assert the expected output
-    expected_output = pd.DataFrame({
+    expected_output = DataFrame({
         'Tarifa por Embarque': [10, 20],
         'id': ['Flow1_Origem1_Destino1', 'Flow2_Origem2_Destino2'],
         'capacity': ['1', '2']
     })
-    pd.testing.assert_frame_equal(processed_prices, expected_output)
+    assert_frame_equal(processed_prices, expected_output)
+
 
 def test_preprocess_distances():
-    # Create a sample DataFrame for testing
-    distances = pd.DataFrame({
+    distances = DataFrame({
         'Fluxo': ['Flow1', 'Flow2'],
         'Estado de Origem': ['MG', 'SP'],
         'Estado de Destino': ['SP', 'MG'],
@@ -43,14 +35,11 @@ def test_preprocess_distances():
         'Origem': ['Origem1', 'Origem2'],
         'Destino': ['Destino1', 'Destino2']
     })
-    # Call the function to preprocess the distances DataFrame
     processed_distances = preprocess_distances(distances)
-
-    # Assert the expected output
-    expected_output = pd.DataFrame({
+    expected_output = DataFrame({
         'Distância em Km': [100, 200],
         'id': ['Flow1_Origem1, MG_Destino1, SP', 'Flow2_Origem2, SP_Destino2, MG'],
         'category': ['MG_SP', 'SP_MG']
     })
-    pd.testing.assert_frame_equal(processed_distances, expected_output)
+    assert_frame_equal(processed_distances, expected_output)
     
